d90e5305e39eb644d173ce4ab583e24767927ac7
dimpulsha <dimpulsha@gmail.com>
2021-10-06 19:56:04 +0500

    add LigaA gulp template

--- gulpfile.js
+++ gulpfile.js
@@ -1,237 +1,101 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const less = require("gulp-less");
-const postcss = require("gulp-postcss");
-const csso = require("postcss-csso");
-const autoprefixer = require("autoprefixer");
-const sync = require("browser-sync").create();
-const del = require("del");
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
 var rename = require("gulp-rename");
-const htmlmin = require("gulp-htmlmin");
-const terser = require("gulp-terser");
-const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
-const svgstore = require("gulp-svgstore");
-const cheerio = require("gulp-cheerio");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore")
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
 
-//Clean
-const clean = () => {
-  return del("build");
-}
-
-exports.clean = clean;
-
-//Copy
-const copy = (done) => {
-  gulp.src(["source/*.ico",
-    "source/fonts/*.{woff,woff2}",
-    "source/manifest.webmanifest",
-    //"source/img/*.svg",
-    //"source/img/background-img/*",
-    //"!source/img/icon-sprite.src",
-    //"!source/img/menu-sprite.src",
-    //"!source/img/decor-sprite.src",
-    "source/img/favicon/*"
-  ], {
-    base: "source"
-  })
-    .pipe(gulp.dest("build"))
-  done();
-}
-
-exports.copy = copy;
-
-//HTML
-
-const htmlMin = () => {
-  return gulp.src("source/**/*.html")
-    .pipe(htmlmin({ collapseInlineTagWhitespace: true }))
-    .pipe(gulp.dest("build"));
-}
-
-exports.htmlMin = htmlMin;
-
-// Styles
-
-const styles = () => {
-  return gulp.src("source/less/style.less")
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer(),
-      csso()
-    ]))
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
-    .pipe(sync.stream());
-}
+    .pipe(server.stream());
+});
 
-exports.styles = styles;
-
-//Script
-
-const jsMin = () => {
-  return gulp.src('source/js/*.js')
-    .pipe(terser())
-    .pipe(rename(function (path) {
-      path.extname = ".min.js";
-    }))
-    .pipe(gulp.dest('build/js'));
-}
-
-exports.jsMin = jsMin;
-
-//optimizeImages
-
-const optimizeImages = () => {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([imagemin.mozjpeg({progressive: true}),
-    imagemin.optipng({ optimizationLevel: 3 }),
-    imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/img"))
-}
-
-exports.optimizeImages = optimizeImages;
-
-
-//copyImages
-
-const copyImages = () => {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(gulp.dest("build/img"))
-}
-
-exports.images = copyImages;
-
-//createWebp
-const createWebp = () => {
-  return gulp.src("source/img/*.{png,jpg}")
-    .pipe(webp({ quality: 85 }))
-    .pipe(gulp.dest("build/img/webp"))
-}
-
-exports.createWebp = createWebp;
-
-//createMenuSprite
-const createMenuSprite = () => {
-  return gulp.src("build/img/menu-sprite.src/*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("menu-sprite.svg"))
-    .pipe(gulp.dest("build/img"))
-}
-
-exports.createMenuSprite = createMenuSprite;
-
-//createSprite
-const createIconSprite = () => {
-  return gulp.src("build/img/icon-sprite.src/*.svg")
-    .pipe(cheerio({
-      run: function ($) {
-        $('[fill]').removeAttr('fill');
-        $('[opacity]').removeAttr('opacity');
-      },
-      parserOptions: { xmlMode: true }
-    }))
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("icon-sprite.svg"))
-    .pipe(gulp.dest("build/img"))
-}
-
-exports.createIconSprite = createIconSprite;
-
-// Server
-
-const server = (done) => {
-  sync.init({
-    server: {
-      baseDir: 'build'
-    },
-    cors: true,
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
     notify: false,
-    ui: false,
+    open: true,
+    cors: true,
+    ui: false
   });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
   done();
-}
+});
 
-exports.server = server;
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
 
-// Reload
+    .pipe(gulp.dest("source/img"));
 
-const reload = (done) => {
-  sync.reload();
-  done();
-}
+});
 
-// Watcher
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
 
-const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/js/*.js", gulp.series("jsMin"));
-  gulp.watch("source/*.html", gulp.series(htmlMin, reload));
-}
+gulp.task("sprite", function () {
+  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite_auto.svg"))
+    .pipe(gulp.dest("build/img"));
+});
 
-//build  - test
-const buildProject = gulp.series(
-  clean,
-  copy,
-  optimizeImages,
-  gulp.parallel(
-    htmlMin,
-    styles,
-    jsMin,
-    createWebp,
-    createMenuSprite,
-    createIconSprite
-  )
-);
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
 
-exports.buildProject = buildProject;
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source//*.ico"
+    ], {
+      base: "source"
+    })
+  .pipe(gulp.dest("build"));
+});
 
-//RunServer -test
+gulp.task("clean", function () {
+  return del("build");
+});
 
-const runProject = gulp.series(
-  buildProject, server, watcher
-);
-
-exports.runProject = runProject;
-
-const build = gulp.series(
-  clean,
-  copy,
-  optimizeImages,
-  gulp.parallel(
-    htmlMin,
-    styles,
-    jsMin,
-    createWebp,
-    createMenuSprite,
-    createIconSprite
-  )
-);
-
-exports.build = build;
-
-exports.default = gulp.series(
-  clean,
-  copy,
-  optimizeImages,
-  gulp.parallel(
-    htmlMin,
-    styles,
-    jsMin,
-    createWebp,
-    createMenuSprite,
-    createIconSprite
-  ),
-  gulp.series(
-    server,
-    watcher)
-);
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("start", gulp.series("build", "server"));
