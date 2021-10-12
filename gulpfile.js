0cb2b373316e7f2d16527e5c928ee393b2dd594b
dimpulsha <dimpulsha@gmail.com>
2021-10-12 22:31:50 +0500

    pp desktop

--- gulpfile.js
+++ gulpfile.js
@@ -97,5 +97,7 @@ gulp.task("clean", function () {
   return del("build");
 });
 
+// gulp.task("build2", gulp.series("clean", "webp", "images", "css", "sprite", "html"));
 gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+// gulp.task("start2", gulp.series("build2", "server"));
 gulp.task("start", gulp.series("build", "server"));
